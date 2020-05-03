0ce04c7c470422b60ca9f5b4e092434078901342
Andy Philpotts <andy@failfree.net>
2020-05-02 21:15:50 -0400

    Convert to Udp, add basic command line parsing

--- src/bin/snmp-test-agent.rs
+++ src/bin/snmp-test-agent.rs
@@ -1,12 +1,29 @@
 extern crate snmp_agent;
+// extern crate clap;
 
+use clap::Clap;
+  
 use std::time::Instant;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = VERSION, author = "Andy P. <andy@failfree.net>")]
+struct Opts {
+    /// Sets a custom port, defaults to 1161
+    #[clap(short = "p", long = "port", default_value = "1161")]
+    port: u16,
+}
+
 fn main() {
+    let opts: Opts = Opts::parse();
+
     println!("SNMP Test Agent started");
     let start_time = Instant::now();
 
-    snmp_agent::run();
+    snmp_agent::run(opts.port);
 
     println!("SNMP Test Agent completed in {}ms", start_time.elapsed().as_millis())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use std::time::Duration;
 use std::io;
 
 use async_std::task;
-use async_std::net::TcpListener;
+use async_std::net::{UdpSocket,SocketAddr};
 use async_std::prelude::*;
 
 use thiserror::Error;
 
-use async_listen::{ListenExt, ByteStream, backpressure, error_hint};
+// use async_listen::{ListenExt, ByteStream, backpressure, error_hint};
 
 #[derive(Error, Debug)]
 pub enum SnmpAgentError {
@@ -16,41 +16,29 @@ pub enum SnmpAgentError {
     Network(#[from] io::Error),
 }
 
-pub fn run() {
+pub fn run(port : u16) {
     println!("SNMP Agent running");
-    if let Err(e) = task::block_on(listen()) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    if let Err(e) = task::block_on(listen(addr)) {
         eprintln!("Error: {}", e);       
     }
 
 }
 
-async fn listen() -> Result<(), SnmpAgentError> {
-    let (_, bp) = backpressure::new(10);
-    let listener = TcpListener::bind("localhost:8080").await?;
-    eprintln!("Accepting connections on localhost:8080");
-    let mut incoming = listener.incoming()
-        .log_warnings(log_accept_error)
-        .handle_errors(Duration::from_millis(500))
-        .backpressure_wrapper(bp);
-    while let Some(stream) = incoming.next().await {
-        task::spawn(async {
-            if let Err(e) = connection_loop(stream).await {
-                eprintln!("Error: {}", e);
-            }
-        });
+async fn listen(address: SocketAddr) -> Result<(), SnmpAgentError> {
+    println!("Binding to port {}", address);
+    let socket = UdpSocket::bind(address).await?;
+    let mut buf = vec![0; 1024];
+
+    loop {
+        let (n, peer) = socket.recv_from(&mut buf).await?;
+        println!("Received {} bytes from {}", n, peer);
+
+        // simple echo
+        let sent = socket.send_to(&buf[..n], peer).await?;
+        println!("Sent {} bytes to {}", sent, peer);
     }
-    Ok(())
-}
-
-async fn connection_loop(mut stream: ByteStream) -> Result<(), io::Error> {
-    println!("Connected from {}", stream.peer_addr()?);
-    task::sleep(Duration::from_secs(5)).await;
-    stream.write_all("hello\n".as_bytes()).await?;
-    Ok(())
-}
-
-fn log_accept_error(e: &io::Error) {
-    eprintln!("Accept error: {}. Sleeping 0.5s. {}", e, error_hint(&e));
+    // Ok(()) 
 }
 
 #[cfg(test)]
